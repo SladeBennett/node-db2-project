f2bca184321d02421ccc7b937bf40e379c217d19
SladeBennett <slade8colt@gmail.com>
2024-08-18 17:16:18 -0600

    cars seed established

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,30 @@
-// STRETCH
+
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 115000,
+        title: 'salvage',
+    },
+    {
+        vin: '1111111111111',
+        make: 'ford',
+        model: 'focus',
+        mileage: 15000,
+        title: 'clean',
+    },
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
